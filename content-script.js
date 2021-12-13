bb1971ffd49f4f1f8c7233f7678d78c7cf6ed257
timwis <tim@timwis.com>
2021-12-13 08:11:14 +0000

    Add 'Join Hybrid' button

--- content-script.js
+++ content-script.js
@@ -1,3 +1,22 @@
+// Add 'Join Hybrid' button
+const loadingIndicatorQuery = "//div[text()='Getting ready...']"
+const joinBtnQuery = "//div[@role='button'][descendant::text()='Join now'][not(@aria-disabled)]"
+const loadingIndicator = getElementByXPath(loadingIndicatorQuery)
+
+if (loadingIndicator) {
+  const loadingObserver = new MutationObserver(function (mutationList, observer) {
+    if (!getElementByXPath(loadingIndicatorQuery)) {
+      // Wait for animation to finish
+      setTimeout(() => {
+        addJoinHybridBtn()
+      }, 500)
+      observer.disconnect()
+    }
+  })
+  loadingObserver.observe(loadingIndicator.parentElement, { childList: true, subtree: true })
+}
+
+// Remove <audio> elements
 const audioObserver = new MutationObserver(function (mutationList, observer) {
   const audioEls = document.getElementsByTagName('audio')
   if (audioEls.length > 0) {
@@ -7,6 +26,7 @@ const audioObserver = new MutationObserver(function (mutationList, observer) {
 
 audioObserver.observe(document.body, { childList: true })
 
+// Keep microphone button muted
 const micBtnPresenceObserver = new MutationObserver(function (mutationList, observer) {
   const micBtn = document.querySelector('button[aria-label*="microphone"]')
 
@@ -28,3 +48,23 @@ const micBtnAttributeObserver = new MutationObserver(function (mutationList, obs
     micBtn.click()
   }
 })
+
+function addJoinHybridBtn () {
+  const joinBtn = getElementByXPath("//div[@role='button'][descendant::text()='Join now']")
+  const presentBtn = getElementByXPath("//div[@role='button'][descendant::text()='Present']")
+  const joinBtnContainer = joinBtn.parentElement
+
+  const hybridBtn = presentBtn.cloneNode(true)
+  hybridBtn.removeAttribute('jsname') // leave jscontroller and jsaction to enable click animation
+  hybridBtn.setAttribute('id', 'join-hybrid-btn')
+  hybridBtn.querySelector('.google-material-icons').innerText = 'mic_off'
+
+  const label = getElementByXPath("//span[text()='Present']/text()", hybridBtn)
+  label.textContent = 'Join hybrid'
+
+  joinBtnContainer.appendChild(hybridBtn)
+}
+
+function getElementByXPath(xpath, context=document) {
+  return document.evaluate(xpath, context, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+}
