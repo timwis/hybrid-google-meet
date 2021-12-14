798e45d094e212c891bf53c1af8776bd7e469ddf
timwis <tim@timwis.com>
2021-12-14 06:42:28 +0000

    Tidy query selectors and wrap 'join hybrid' button in onload

--- content-script.js
+++ content-script.js
@@ -1,19 +1,27 @@
-// Add 'Join Hybrid' button
-const loadingIndicatorQuery = "//div[text()='Getting ready...']"
-const joinBtnQuery = "//div[@role='button'][descendant::text()='Join now'][not(@aria-disabled)]"
-const loadingIndicator = getElementByXPath(loadingIndicatorQuery)
+const queries = {
+  loadingIndicator: "//div[text()='Getting ready...']",
+  joinBtn: "//div[@role='button'][descendant::text()='Join now'][not(@aria-disabled)]",
+  presentBtn: "//div[@role='button'][descendant::text()='Present']",
+  presentLabel: "//span[text()='Present']/text()",
+  micBtn: "button[aria-label*='microphone']" // DOMString
+}
 
-if (loadingIndicator) {
-  const loadingObserver = new MutationObserver(function (mutationList, observer) {
-    if (!getElementByXPath(loadingIndicatorQuery)) {
-      // Wait for animation to finish
-      setTimeout(() => {
-        addJoinHybridBtn()
-      }, 500)
-      observer.disconnect()
-    }
-  })
-  loadingObserver.observe(loadingIndicator.parentElement, { childList: true, subtree: true })
+// Add 'Join Hybrid' button
+window.onload = function () {
+  const loadingIndicator = getElementByXPath(queries.loadingIndicator)
+
+  if (loadingIndicator) {
+    const loadingObserver = new MutationObserver(function (mutationList, observer) {
+      if (!getElementByXPath(queries.loadingIndicator)) {
+        // Wait for animation to finish
+        setTimeout(() => {
+          addJoinHybridBtn()
+        }, 500)
+        observer.disconnect()
+      }
+    })
+    loadingObserver.observe(loadingIndicator.parentElement, { childList: true, subtree: true })
+  }
 }
 
 // Remove <audio> elements
@@ -28,7 +36,7 @@ audioObserver.observe(document.body, { childList: true })
 
 // Keep microphone button muted
 const micBtnPresenceObserver = new MutationObserver(function (mutationList, observer) {
-  const micBtn = document.querySelector('button[aria-label*="microphone"]')
+  const micBtn = document.querySelector(queries.micBtn)
 
   if (micBtn) {
     if (micBtn.dataset.isMuted !== 'true') {
@@ -50,8 +58,8 @@ const micBtnAttributeObserver = new MutationObserver(function (mutationList, obs
 })
 
 function addJoinHybridBtn () {
-  const joinBtn = getElementByXPath("//div[@role='button'][descendant::text()='Join now']")
-  const presentBtn = getElementByXPath("//div[@role='button'][descendant::text()='Present']")
+  const joinBtn = getElementByXPath(queries.joinBtn)
+  const presentBtn = getElementByXPath(queries.presentBtn)
   const joinBtnContainer = joinBtn.parentElement
 
   const hybridBtn = presentBtn.cloneNode(true)
@@ -59,7 +67,7 @@ function addJoinHybridBtn () {
   hybridBtn.setAttribute('id', 'join-hybrid-btn')
   hybridBtn.querySelector('.google-material-icons').innerText = 'mic_off'
 
-  const label = getElementByXPath("//span[text()='Present']/text()", hybridBtn)
+  const label = getElementByXPath(queries.presentLabel, hybridBtn)
   label.textContent = 'Join hybrid'
 
   joinBtnContainer.appendChild(hybridBtn)
